a0e7e8508286412bd0b6dcb6b928bc6598f84ebd
Andras Elso <ea333@freemail.hu>
2010-01-25 17:13:40 +0100

    Next DHT announce query over rpc

--- daemon/remote.c
+++ daemon/remote.c
@@ -318,6 +318,7 @@ static const char * details_keys[] = {
     "corruptEver",
     "creator",
     "dateCreated",
+    "dhtAnnounceTime"
     "doneDate",
     "downloadDir",
     "downloadedEver",
@@ -1271,6 +1272,11 @@ printDetails( tr_benc * top )
                         "  Web Seeds: downloading from %" PRId64 " of %"
                         PRId64
                         " web seeds\n", i, n );
+            }
+            if( tr_bencDictFindInt( t, "dhtAnnounceTime", &i ) && i )
+            {
+                const time_t tt = i;
+                printf( "  Next DHT announce:  %s", ctime( &tt ) );
             }
             printf( "\n" );
 
