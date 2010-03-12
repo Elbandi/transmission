Elbandi/transmission

commit c94da3f0f66eb8f3aff8cc570698b0927f318fbb

(1.9x gtk) #3033 "wrong calls to GtkFileChooserDialog" -- fixed in 1.9x for 1.92
git-svn-id: svn://svn.transmissionbt.com/Transmission/branches/1.9x@10341 f4695dd4-2c0a-0410-b89c-da849a56a58e

gtk/add-dialog.c
@@ -337,8 +337,8 @@ addSingleTorrentDialog( GtkWindow * parent, TrCore * core, tr_ctor * ctor )
     ++col;
     w = gtk_file_chooser_button_new( _( "Select Destination Folder" ),
                                      GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER );
-    if( !gtk_file_chooser_set_filename( GTK_FILE_CHOOSER( w ),
-                                        data->downloadDir ) )
+    if( !gtk_file_chooser_set_current_folder( GTK_FILE_CHOOSER( w ),
+                                              data->downloadDir ) )
         g_warning( "couldn't select '%s'", data->downloadDir );
     list = get_recent_destinations( );
     for( walk = list; walk; walk = walk->next )

gtk/makemeta-ui.c
@@ -403,7 +403,7 @@ make_meta_ui( GtkWindow  * parent, TrCore * core )
 
         str = _( "Sa_ve to:" );
         w = gtk_file_chooser_button_new( NULL, GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER );
-        gtk_file_chooser_set_filename( GTK_FILE_CHOOSER( w ), getDefaultSavePath( ) );
+        gtk_file_chooser_set_current_folder( GTK_FILE_CHOOSER( w ), getDefaultSavePath( ) );
         ui->destination_chooser = w;
         hig_workarea_add_row( t, &row, str, w, NULL );
 

gtk/relocate.c
@@ -134,7 +134,7 @@ gtr_relocate_dialog_new( GtkWindow * parent, tr_torrent * tor )
     if( previousLocation == NULL )
         previousLocation = g_strdup( pref_string_get( TR_PREFS_KEY_DOWNLOAD_DIR ) );
     w = gtk_file_chooser_button_new( _( "Set Torrent Location" ), GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER );
-    gtk_file_chooser_set_filename( GTK_FILE_CHOOSER( w ), previousLocation );
+    gtk_file_chooser_set_current_folder( GTK_FILE_CHOOSER( w ), previousLocation );
     g_object_set_data( G_OBJECT( d ), "chooser", w );
     hig_workarea_add_row( t, &row, _( "Torrent _location:" ), w, NULL );
     w = gtk_radio_button_new_with_mnemonic( NULL, _( "_Move from the current folder" ) );

commit 0466e76f0bdd89a30b11ca7ecad3d185c812602e

(1.9x gtk) #3033 "wrong calls to GtkFileChooserDialog" -- fix minor regression in the 'add torrent' dialog caused by r10341
git-svn-id: 10357@f4695dd4-2c0a-0410-b89c-da849a56a58e

gtk/add-dialog.c
@@ -155,6 +155,7 @@ updateTorrent( struct AddData * o )
         tr_torrent * tor = tr_torrent_handle( o->gtor );
         tr_torrentSetDownloadDir( tor, o->downloadDir );
         file_list_set_torrent( o->list, tr_torrentId( tor ) );
+        tr_torrentVerify( tor );
     }
 }
 
@@ -166,12 +167,10 @@ updateTorrent( struct AddData * o )
  * metadata when that happens.
  */
 static void
-sourceChanged( GtkFileChooserButton * b,
-               gpointer               gdata )
+sourceChanged( GtkFileChooserButton * b, gpointer gdata )
 {
     struct AddData * data = gdata;
-    char *           filename = gtk_file_chooser_get_filename(
-         GTK_FILE_CHOOSER( b ) );
+    char * filename = gtk_file_chooser_get_filename( GTK_FILE_CHOOSER( b ) );
 
     /* maybe instantiate a torrent */
     if( data->filename || !data->gtor )
@@ -210,29 +209,16 @@ sourceChanged( GtkFileChooserButton * b,
 }
 
 static void
-verifyRequested( GtkButton * button UNUSED, gpointer gdata )
+downloadDirChanged( GtkFileChooserButton * b, gpointer gdata )
 {
     struct AddData * data = gdata;
-
-    if( data->gtor )
-        tr_torrentVerify( tr_torrent_handle( data->gtor ) );
-}
-
-static void
-downloadDirChanged( GtkFileChooserButton * b,
-                    gpointer               gdata )
-{
-    char *           fname = gtk_file_chooser_get_filename(
-         GTK_FILE_CHOOSER( b ) );
-    struct AddData * data = gdata;
+    char * fname = gtk_file_chooser_get_filename( GTK_FILE_CHOOSER( b ) );
 
     if( fname && ( !data->downloadDir || strcmp( fname, data->downloadDir ) ) )
     {
         g_free( data->downloadDir );
         data->downloadDir = g_strdup( fname );
-
         updateTorrent( data );
-        verifyRequested( NULL, data );
     }
 
     g_free( fname );