ff5a18e49e70d9a51b0fbe1919dd03953da179dc
Andras Elso <ea333@freemail.hu>
2010-08-17 17:43:24 +0200

    Can add peer to torrent

--- libtransmission/rpcimpl.c
+++ libtransmission/rpcimpl.c
@@ -28,6 +28,7 @@
 #include "completion.h"
 #include "fdlimit.h"
 #include "json.h"
+#include "peer-mgr.h"
 #include "rpcimpl.h"
 #include "session.h"
 #include "torrent.h"
@@ -1061,6 +1062,31 @@ removeTrackers (tr_torrent * tor, tr_benc * ids)
     return errmsg;
 }
 
+static void
+addPeer( tr_torrent * tor,
+         const char * str )
+{
+    const char * delim = strchr( str, ':' );
+    if( delim )
+    {
+        tr_pex pex;
+        char * host;
+        int    port = atoi( delim + 1 );
+	
+        if ( port < 0 || port > USHRT_MAX )
+            return;
+
+        host = tr_strndup( str, delim - str );
+        if ( tr_address_from_string( &pex.addr, host ) )
+        {
+            pex.port = htons( port );
+            tr_peerMgrAddPex( tor, TR_PEER_FROM_PEX , &pex, -1 );
+        }
+        tr_free( host );
+    }
+    return;
+}
+
 static const char*
 torrentSet (tr_session               * session,
             tr_benc                  * args_in,
@@ -1077,11 +1103,14 @@ torrentSet (tr_session               * session,
     {
         int64_t      tmp;
         double       d;
+        const char * str;
         tr_benc *    files;
         tr_benc *    trackers;
         bool         boolVal;
         tr_torrent * tor = torrents[i];
 
+        if (tr_bencDictFindStr (args_in, "add-peer", &str))
+            addPeer (tor, str);
         if (tr_bencDictFindInt (args_in, "bandwidthPriority", &tmp))
             if (tr_isPriority (tmp))
                 tr_torrentSetPriority (tor, tmp);
